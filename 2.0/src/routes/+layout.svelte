bbacd6744ccf21b423644b8aea21b4cac4d427ea
Tedd Lee <tedd0328@gmail.com>
2023-12-25 17:31:43 -0500

    Initial Commit (SvelteKit, Vite, Tailwind)

---
+++ 2.0/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import "../app.css";
+</script>
+    
+<nav class="bg-stone-200 h-20 py-3 px-12 mb-10 flex items-center place-content-between">
+    <img src="/banner.svg" alt="greencat" class="h-8 inline-block"/>
+    <div class="inline-flex justify-end space-x-8 align-center flex-nowrap w-1/3">
+        <a href='/'>Home</a>
+        <a href='/about'>About</a>
+        <a href='/blog'>Blog</a>
+        <a href='/portfolio'>Portfolio</a>
+        <a href='/contact'>Contact</a>
+    </div>
+    
+</nav>
+
+<div class="block px-48">
+    <slot />
+</div>
---
+++ 2.0/src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="block h-20"></div>
+
+<h1 class="text-6xl font-light mb-8 text-stone-800">Hi, I'm <span class="whitespace-nowrap underline underline-offset-8 decoration-2 decoration-green-500">Tedd Lee</span></h1>
+<p>Can't find what you're looking for? Poke around on <a href="https://aGreenCat.github.io/1.0/" class="text-green-500">version 1.0</a> of my website.</p>
